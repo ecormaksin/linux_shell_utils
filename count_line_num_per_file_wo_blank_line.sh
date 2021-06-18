83d21b738e577bf990d2de712254df325a9cb684
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-06-18 15:14:32 +0900

    first commit

---
+++ count_line_num_per_file_wo_blank_line.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "対象のディレクトリを指定してください。"
+    exit 1
+fi
+
+TARGET_DIR_PATH=$1
+
+
+if [ -z "${TARGET_DIR_PATH}" ]; then
+    echo "対象のディレクトリを指定してください。"
+    exit 1
+fi
+
+if [ -e "${TARGET_DIR_PATH}" ]; then
+    # パスが存在する
+    : # 何もしない
+else
+    echo "存在するパスを指定してください。"
+    exit 1
+fi
+
+if [ -d "${TARGET_DIR_PATH}" ]; then
+    # パスはディレクトリ
+    : # 何もしない
+else
+    echo "ディレクトリを指定してください。"
+    exit 1
+fi
+
+find "${TARGET_DIR_PATH%/}/" -type f 2>/dev/null | while read -r FILE_PATH
+do
+
+    TEMP_FILE_PATH="${FILE_PATH}_tmp"
+    cp "${FILE_PATH}" "${TEMP_FILE_PATH}"
+    
+    sed -i '/^$/d' "${TEMP_FILE_PATH}" 
+    
+    LINE_NUM=`cat "${TEMP_FILE_PATH}" | wc -l`
+    
+    echo "${FILE_PATH} ${LINE_NUM}"
+    
+    rm -f "${TEMP_FILE_PATH}"
+done
+
+exit $?
